0b0ade4af7e62e2ef4442e080d9a6c9e6e97c1d2
zmilonas <zachary@milonas.pl>
2021-04-24 23:05:17 +0200

    Update case of App

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {render, fireEvent, waitFor, screen, cleanup} from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
-import App from './app';
+import App from './App';
 
 const i18nMock = {
     t: (s) => (s),
--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import * as ReactDOM from "react-dom";
 import * as i18n from "./translations";
 import * as React from "react";
 import "react-datepicker/dist/react-datepicker.css";
-import App from "./app";
+import App from "./App";
 
 ReactDOM.render(React.createElement(App, { i18n }, null), document.getElementById("app"));
