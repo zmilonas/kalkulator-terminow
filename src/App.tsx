0e547d6e8b1e9af75122889d1937e2377a093fe3
zmilonas <zachary@milonas.pl>
2021-04-29 14:48:33 +0200

    WIP Style, add text

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import {ChakraProvider, Container, Grid, GridItem, Heading, Input,} from "@chakra-ui/react";
+import {ChakraProvider, Container, extendTheme, Grid, GridItem, Heading, Input, Link, Text} from "@chakra-ui/react";
 import DatePicker from "react-datepicker";
 import {countDates} from "./count-dates";
 import {defaultStartDate, DurationCtx, SelectedDuration, useDuration} from "./store";
@@ -22,18 +22,38 @@ type AppDependencies = {
   i18n: typeof i18n;
 }
 
+const theme = extendTheme({
+    colors: {
+        muted: 'gray.500',
+        accent: 'teal.500',
+    },
+})
+
+const Helmet = () => (
+    <>
+        <Heading as={'h1'} size={'xl'}>Kalkulator terminów</Heading>
+        <Text>Obliczenie terminu według zasad określonych w KPA</Text>
+        <Text fontSize={'sm'}>Podstawą prawną jest Art. 57, par. 1 - 4 ustawy z dnia 14 czerwca 1960 r. Kodeks
+            postępowania administracyjnego (t.j. Dz. U. z 2020 r. poz. 256 z późn. zm.). Bardzo podobne zasady
+            obliczania terminów obowiązują w innych gałęziach prawa - cywilnym i karnym w związku z tym powinny być one
+            możliwe do stosowania zamiennie.</Text>
+        <Text fontSize={'sm'}>Kalkulator nie daje gwarancji poprawności danego terminu ale zdecydowania pomaga w nauce,
+            zrozumieniu i ew. szybszym sprawdzeniu aniżeli ręczne liczenie takiej daty.</Text>
+        <Text fontSize={'md'}>Stworzone przez <Link href={"https://zachary.milonas.pl"}>Zacharego Milonasa</Link>, zobacz na
+            GitHub: <Link
+                href={"https://github.com/zmilonas/kalkulator-terminow"}>github.com/zmilonas/kalkulator-terminow</Link></Text>
+    </>
+)
+
 export default function App({ i18n }: AppDependencies) {
   const [duration, setDuration] = useDuration();
   const [startDate, setStartDate] = React.useState<Date>(defaultStartDate);
   const [resultDate, explanations] = calculate(startDate, duration);
 
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Container centerContent maxWidth={"50rem"}>
-        <Heading role={"heading"}>{i18n.t('Limitation calculator')}</Heading>
-        <Heading size="xs">
-          Obliczenie terminu według zasad określonych w KPA
-        </Heading>
+        <Helmet />
         <DurationCtx.Provider value={{ duration, setDuration }}>
           <DateHelperContext.Provider value={{
             format: i18n.formatDate,
